5304b533dce1a199b9d32e302b45c2cd7dea384e
Michał Marciniak <michal.mar725@gmail.com>
2025-09-30 12:03:09 +0200

    finish base functions
    - to do: display result when pressing another operator instead of '='

--- calculator.js
+++ calculator.js
@@ -1,46 +1,142 @@
+const digitButtons = document.querySelectorAll('.digit');
+const operationButtons = document.querySelectorAll('.operation');
 
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
+const percentButton = document.querySelector("#percent");
+const enterButton = document.querySelector("#enter");
+const dotButton = document.querySelector('.dot');
+const display = document.querySelector("#text-area");
 
+display.textContent = 0;
 
+let num1 = 0;
+let num2 = 0;
+let operator = null;
+let nextNumber = false;
+let repeat = false; // when just pressing enter without changing op or num2
+display.textContent = 0;
+
+clearButton.addEventListener('click', () => clear());
+
+deleteButton.addEventListener('click', () => del());
+
+percentButton.addEventListener('click', () => percent());
+
+dotButton.addEventListener('click', () => {
+    if (!display.textContent.includes('.')){
+        display.textContent += '.';
+    }
+})
+
+operationButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (nextNumber) {
+            operator = button.id;
+            return;
+        }
+        operator = button.id;
+        num1 = +display.textContent;
+        nextNumber = true;
+        repeat = false;
+    })
+})
+
+enterButton.addEventListener('click', () => {
+    console.log(`${num1} ${num2} ${operator}`);
+    operate(num1, num2, operator);
+    num1 = +display.textContent;
+})
+
+digitButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (display.textContent === '0') {
+            display.textContent = button.textContent;
+            return;
+        }
+        if (nextNumber) {
+            display.textContent = button.textContent;
+            nextNumber = false;
+        } else {
+            display.textContent += button.textContent;
+        }
+        repeat=false;
+        
+    })
+})
+
+function clear() {
+    num1 = 0;
+    num2 = 0;
+    operator = null;
+    nextNumber = false;
+    repeat = false;
+    display.textContent = 0;
+}
+
+function del() {
+    let string = display.textContent;
+    if (string.length > 1) {
+        display.textContent = string.slice(0, -1);
+    } else {
+        display.textContent = 0;
+    }
+}
+
+function percent() {
+    console.log(display.textContent);
+    if (nextNumber) {
+        return;
+    }
+    if (display.textContent != 0) {
+        display.textContent = +display.textContent/100;
+    }
+}
 
 function add(a,b) {
-    return a+b;
+    display.textContent = a+b;
 }
 
 function sub(a,b) {
-    return a-b;
+    display.textContent = a-b;
 }
 
 function divide(a,b) {
-    if (b == 0) {
-        return 'Universe exploded.'
+    if (b == '0') {
+        clear()
+        display.textContent ='Universe exploded.';
+        nextNumber = true;
+    } else {
+        display.textContent = a/b;
     }
-    return a/b;
+
 }
 
 function mult(a,b) {
-    return a*b;
+    display.textContent = a*b;
 }
 
 function operate(num1, num2, operator) {
+    if (!repeat) {
+        num2 = +display.textContent;
+    }
+    repeat=true;
+    nextNumber = true;
+    
     switch(operator) {
-        case '+': 
-            return add(num1, num2);
+        case 'add': 
+            add(num1, num2);
             break;    
-        case '-':
-            return sub(num1, num2);
+        case 'sub':
+            sub(num1, num2);
             break;
-        case '*':
-            return mult(num1, num2);
+        case 'mult':
+            mult(num1, num2);
             break;
-        case '/':
-            return divide(num1, num2);
+        case 'divide':
+            divide(num1, num2);
             break;
         default:
-            console.log("How did you get here?");
+            // action without operator - number and enter
     }
 }
-
-console.log(divide(10, 0));
-
-console.log(operate(2, 10, '*'));
-console.log(operate(2, 0, '/'));
